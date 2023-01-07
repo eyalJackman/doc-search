f9ffc4aa364845b027fd404ea9ae80e2d545abd1
eyalJackman <eyal.jackman@gmail.com>
2023-01-07 14:39:28 -0800

    Updated Inverted Index for ease with BM25 and QL

--- bm25.py
+++ bm25.py
@@ -1,4 +1,9 @@
+from typing import List
+
 # Typical TREC Values:
 k1 = 1.2
 k2 = 100
 b = 0.75
+
+def bm25(query: List[str], document):
+    pass
--- inverted_index.py
+++ inverted_index.py
@@ -4,11 +4,12 @@ from tokenizer import tokenize, tokenize_word
 
 
 # Inverted Index: Dict {
-#   _numdocs: int
+#   _totaldocs: int
 #   _collection_cnt: int --- Total words in collection (For QL)
 #   ...(term): {
-#                  _list: [..., [doc_id, cnt, [...positions] ]
-#                  _count: int --- Total word count in document (For QL/BM25)
+#                  _docs: {doc_id: [cnt, {...positions}] , ... }
+#                  _count: int   --- Total occurrences of (term) in collection
+#                  _numdocs: int --- Total number of docs that contain (term)
 #               }
 #   }
 
@@ -18,7 +19,7 @@ def build_index(collection: "List[Dict[str, str]]") -> "Dict[str, str]":
     Creates inverted index from a list of documents --- InvertedIndex structure above
     """
     inv_index = dict()
-    inv_index["_numdocs"] = 0
+    inv_index["_totaldocs"] = 0
     inv_index["_collection_cnt"] = 0
     for document in collection:
         assert "id" in document and "text" in document
@@ -47,16 +48,20 @@ def build_index(collection: "List[Dict[str, str]]") -> "Dict[str, str]":
         # Transfer to inverted index
         for term in doc_dict:
             document_info = doc_dict[term] # [docid, count, [positions]]
+            docid, count, positions = document_info
             term_cnt = document_info[1]
             inv_index["_collection_cnt"] += term_cnt
             if term not in inv_index:
-                inv_index[term] = {"_list": [document_info]}
+                inv_index[term] = {"_docs": {docid: [count, set(positions)]}}
                 inv_index[term]["_count"] = term_cnt
+                inv_index[term]["_numdocs"] = 1
             else:
-                # Implement binary search to put these in order
-                inv_index[term]["_list"].append(document_info)
+                # Implement binary search to put these in order\
+                inv_index[term]["_docs"][docid] = [count, set(positions)]
+                # inv_index[term]["_list"].append(document_info)
                 inv_index[term]["_count"] += term_cnt
-        inv_index["_numdocs"] += 1
+                inv_index[term]["_numdocs"] += 1
+        inv_index["_totaldocs"] += 1
 
     return inv_index
 
@@ -71,7 +76,7 @@ def merge_index(index: "InvertedIndex", sub_index: "InvertedIndex"):
 
 
 
-dict1 = {"id": 1203421, "text": "here we go jesus"}
+dict1 = {"id": 1203421, "text": "here we go jesus we"}
 dict2 = {"id": 123125125, "text": "another document for the books we have"}
 # print(dict1['text'])
 # print(type(dict1))
